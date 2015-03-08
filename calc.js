c3054e8fc8c9dfb72fc448eb7c85febd7d62291b
Julian <julian.deocampo@gmail.com>
2015-03-07 18:25:07 -0700

    added clear button; tidied up

--- calc.js
+++ calc.js
@@ -1,66 +1,71 @@
 var num1 = "",
-num2 = "",
-ans = "";
-var operand = "";
+    num2 = "",
+    ans = "",
+    operand = "";
 
 $(".num").click(function () {
-    if(operand === ""){
-    	if (ans === ""){
-	    	num1 = num1 + ($(this).text());
-	    	alert(num1);
-	    	}
-    	else {
-    		num1 = $(this).text();
-    		ans = "";
-    		alert(num1);
-    	}
+    if (operand === "") {
+        if (ans === "") {
+            num1 = num1 + ($(this).text());
+            alert(num1);
+        } else {
+            num1 = $(this).text();
+            ans = "";
+            alert(num1);
+        }
+    } else {
+        num2 = num2 + ($(this).text());
+        alert(num2);
     }
-    else{
-    	num2 = num2 + ($(this).text());
-    	alert(num2);
-    }
-    });
+});
 
 $(".operand").click(function () {
     alert($(this).text());
     operand = $(this).text();
-    });
-
-$("#equals").click(function () {
-	if (num2 === "")
-		num2 = num1;
-    switch (operand){
-	    case "+":
-	    	num1 = add(num1, num2);
-	    	alert(num1);
-	    	num2 = "";
-	    	operand = "";
-	    	break;
-	    case "-":
-	    	num1 = subtract(num1, num2);
-	    	alert(num1);
-	    	num2 = "";
-	    	operand = "";
-	    	break;
-	    case "*":
-	    	num1 = multiply(num1, num2);
-	    	alert(num1);
-	    	num2 = "";
-	    	operand = "";
-	    	break;
-	    case "/":
-	    	num1 = divide(num1, num2);
-	    	alert(num1);
-	    	num2 = "";
-	    	operand = "";
-	    	break;
-	    default:
-	    break;
-	}
 });
 
+$("#equals").click(function () {
+    if (num2 === "") num2 = num1;
+    switch (operand) {
+        case "+":
+            num1 = add(num1, num2);
+            alert(num1);
+            num2 = "";
+            operand = "";
+            break;
+        case "-":
+            num1 = subtract(num1, num2);
+            alert(num1);
+            num2 = "";
+            operand = "";
+            break;
+        case "*":
+            num1 = multiply(num1, num2);
+            alert(num1);
+            num2 = "";
+            operand = "";
+            break;
+        case "/":
+            num1 = divide(num1, num2);
+            alert(num1);
+            num2 = "";
+            operand = "";
+            break;
+        default:
+            break;
+    }
+});
+
+$("#clear").click(function () {
+    num1 = "";
+    num2 = "";
+    operand = "";
+    ans = "";
+});
+
+
 function add(var1, var2) {
-	ans = (parseInt(var1) + parseInt(var2)).toString();
+    ans = (parseInt(var1) + parseInt(var2)).toString();
     return ans;
 }
 
