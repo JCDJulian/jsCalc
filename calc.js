c471c31d22cb0e5aef6a2197893eb293d0f2be56
Julian <julian.deocampo@gmail.com>
2015-03-07 23:11:36 -0700

    GUI implemented, display working

--- calc.js
+++ calc.js
@@ -7,53 +7,55 @@ $(".num").click(function () {
     if (operand === "") {
         if (ans === "") {
             num1 = num1 + ($(this).text());
-            alert(num1);
         } else {
             num1 = $(this).text();
             ans = "";
-            alert(num1);
         }
     } else {
         num2 = num2 + ($(this).text());
-        alert(num2);
+    }
+    if(operand === ""){
+        $("#display").html(num1);
+    }
+    else{
+        $("#display").html(num1 + operand + num2);
     }
 });
 
 $(".operand").click(function () {
-    alert($(this).text());
     operand = $(this).text();
+    num2 = "";
+    $("#display").html(num1 + operand);
 });
 
 $("#equals").click(function () {
-    if (num2 === "") num2 = num1;
+    console.log("Num1 is " + num1 + " and Num2 is " + num2);
+    if (num1 === "") num1 = num2;
     switch (operand) {
         case "+":
             num1 = add(num1, num2);
-            alert(num1);
-            num2 = "";
-            operand = "";
+            //num2 = "";
+            //operand = "";
             break;
         case "-":
             num1 = subtract(num1, num2);
-            alert(num1);
-            num2 = "";
-            operand = "";
+            //num2 = "";
+            //operand = "";
             break;
         case "*":
             num1 = multiply(num1, num2);
-            alert(num1);
-            num2 = "";
-            operand = "";
+            //num2 = "";
+            //operand = "";
             break;
         case "/":
             num1 = divide(num1, num2);
-            alert(num1);
-            num2 = "";
-            operand = "";
+            //num2 = "";
+            //operand = "";
             break;
         default:
             break;
     }
+    $("#display").html(ans);
 });
 
 $("#clear").click(function () {
@@ -61,6 +63,8 @@ $("#clear").click(function () {
     num2 = "";
     operand = "";
     ans = "";
+    $("#display").html("0");
+
 });
 
 
@@ -70,13 +74,16 @@ function add(var1, var2) {
 }
 
 function multiply(var1, var2) {
-    return var1 * var2;
+    ans = (parseInt(var1) * parseInt(var2)).toString();
+    return ans;
 }
 
 function divide(var1, var2) {
-    return var1 / var2;
+    ans = (parseInt(var1) / parseInt(var2)).toString();
+    return ans;
 }
 
 function subtract(var1, var2) {
-    return (var1 - var2);
+    ans = (parseInt(var1) - parseInt(var2)).toString();
+    return ans;
 }
